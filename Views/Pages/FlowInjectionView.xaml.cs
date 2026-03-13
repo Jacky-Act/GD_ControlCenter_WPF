3f8198692532148c1ac3ce1716e0cab265ef742d
XLG <2506143084@qq.com>
2026-03-13 16:19:50 +0800

    流动注射页面

---
+++ Views/Pages/FlowInjectionView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using GD_ControlCenter_WPF.Models.Messages;
+using System.Windows.Controls;
+
+namespace GD_ControlCenter_WPF.Views.Pages
+{
+    public partial class FlowInjectionView : UserControl
+    {
+        private ScottPlot.Plottables.DataLogger? _dataLogger;
+
+        public FlowInjectionView()
+        {
+            InitializeComponent();
+
+        }
+
+      
+    }
+}
